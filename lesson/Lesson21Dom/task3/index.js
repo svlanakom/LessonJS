0018ce9520689b71916c3947da3e603c057c7eff
svlanakom <svetakom.222@gmail.com>
2021-09-08 17:50:46 +0300

    Lesson21task3

---
+++ lesson/Lesson21Dom/task3/index.js
@@ -0,0 +1,13 @@
+function getItemList() {
+  const elementList = document.querySelectorAll(".technology");
+  console.dir(elementList);
+  return elementList;
+}
+getItemList();
+
+function getItemsArray() {
+  const elementsArray = document.querySelectorAll(".tool");
+  console.dir(elementsArray);
+  return Array.From.elementsArray;
+}
+getItemsArray();
