96ce3322b1bc1990741f832ff0b1f652b821b584
svlanakom <svetakom.222@gmail.com>
2021-09-08 17:32:00 +0300

    lesson21task2

--- lesson/Lesson21Dom/task2/index.js
+++ lesson/Lesson21Dom/task2/index.js
@@ -1,10 +1,10 @@
-function getTitleElement() {
+export function getTitleElement() {
   const result = document.querySelector(".title");
 
   console.dir(result);
   return result;
 }
-function getInputElement() {
+export function getInputElement() {
   const result = document.querySelector('[type="text"]');
   console.dir(result);
   return result;
