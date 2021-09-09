3efb74671a69a6b9a353f0fa0dd70186387d1724
svlanakom <svetakom.222@gmail.com>
2021-09-09 19:41:22 +0300

    Lesson21task9

---
+++ lesson/Lesson21Dom/task10/index.js
@@ -0,0 +1,11 @@
+function finishForm() {
+  const form = document.querySelector(".login-form");
+
+  const input = document.createElement("input");
+  input.setAttribute("type", "text");
+  input.setAttribute("name", "login");
+  form.prepend(input);
+  document.querySelector("[name=password]").setAttribute("type", "password");
+}
+
+finishForm();
