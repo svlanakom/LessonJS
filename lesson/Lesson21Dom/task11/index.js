e8f26a1f4d0e17411ea19a789f409f93db8db046
svlanakom <svetakom.222@gmail.com>
2021-09-10 13:30:57 +0300

    Lesson21task11.2

--- lesson/Lesson21Dom/task11/index.js
+++ lesson/Lesson21Dom/task11/index.js
@@ -8,7 +8,7 @@ export function manageClasses() {
   two.classList.remove("selected");
   three.classList.toggle("three_done");
 
-  if (four.includes("some-class")) {
+  if (four.outerHTML.includes("some-class")) {
     four.classList.add("another-class");
   }
 }
