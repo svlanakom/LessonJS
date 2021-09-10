ce41155970190d245270da7f7cfbcb046e4954f0
svlanakom <svetakom.222@gmail.com>
2021-09-10 15:08:37 +0300

    Lesson21task12

---
+++ lesson/Lesson21Dom/task12/index.js
@@ -0,0 +1,8 @@
+function squaredNumbers() {
+  const elementsList = document.querySelectorAll(".number");
+  elementsList.forEach((el) => {
+    const value = el.getAttribute("data-number");
+    el.setAttribute("data-squared-number", value * value);
+  });
+}
+squaredNumbers();
