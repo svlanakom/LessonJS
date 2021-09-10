c2c34467001263fb5263f2c8379c69f4cca05ccb
svlanakom <svetakom.222@gmail.com>
2021-09-10 15:23:52 +0300

    Lesson21task13

---
+++ lesson/Lesson21Dom/task13/index.js
@@ -0,0 +1,5 @@
+function getSection(num) {
+  const spanItem = document.querySelector(`span[data-number='${num}']`);
+  return spanItem.closest(".box").getAttribute("data-section");
+}
+// getSection(1);
