4efe0626f5ea44ecfc0a553262b27ce36d32cab9
svlanakom <svetakom.222@gmail.com>
2021-09-10 19:44:42 +0300

    lesson22task5

---
+++ lesson/Lesson22/task5/index.js
@@ -0,0 +1,17 @@
+const but = document.querySelector(".search__btn");
+const inp = document.querySelector(".search__input");
+
+const act = () => {
+  console.log(inp.value);
+};
+
+but.addEventListener("click", act);
+
+// const btn = document.querySelector(".search__btn");
+// const input = document.querySelector(".search__input");
+
+// const action = () => {
+//   console.log(input.value);
+// };
+
+// btn.addEventListener("click", action);
