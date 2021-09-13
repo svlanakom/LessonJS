4a57287a6e2dc9f738cb240d4646a372881869e9
svlanakom <svetakom.222@gmail.com>
2021-09-13 11:53:24 +0300

    kod16this

---
+++ kod/task16this/index.js
@@ -0,0 +1,97 @@
+// "use strict";
+
+const timer = {
+  secondPassed: 0,
+  minsPassed: 0,
+  timerId: null,
+
+  startTimer() {
+    console.log(this);
+    // lose context case #2
+    // [1, 2, 4].filter(function () {
+    //   console.log("filter this " + this);
+    // });
+
+    this.timerId = setInterval(() => {
+      console.log(this);
+
+      this.secondPassed += 1;
+      if (this.secondPassed === 60) {
+        this.minsPassed += 1;
+        this.secondPassed = 0;
+      }
+    }, 1000);
+
+    //   fix context with bind
+    // const callbackFunc = function () {
+    //   console.log(this);
+    //   this.secondPassed += 1;
+    // };
+    // const bindCallbackFunc = callbackFunc.bind(this);
+    // setInterval(bindCallbackFunc, 1000);
+
+    //      function filter (callback) {
+    //       ...
+    //       ...
+    //      callback()
+    //     ...
+    //   }
+
+    //      function setInterval (callback) {
+    //       ...
+    //       ...
+    //      window.callback()
+    //      ..
+    //   }
+  },
+
+  stopTimer() {
+    clearInterval(this.timerId);
+  },
+
+  getTime() {
+    let secondsToShow = this.secondsPassed;
+    if (this.secondsPassed < 10) {
+      secondsToShow = "0" + this.secondsPassed;
+    }
+    console.log(`${this.minsPassed}:${secondsToShow}`);
+    return `${this.minsPassed}:${secondsToShow}`;
+  },
+
+  resetTimer() {
+    this.secondsPassed = 0;
+    this.minsPassed = 0;
+  },
+};
+
+// context
+// 1. know when context is lost (2-3 cases)
+// 2. fix context
+//  с помощью контекста мы можем использовать результат первой функции в других функциях
+// механизм расшарить даааные между функциями и разные функции имеют к этому доступ
+// помогает изолировать данные. можно четко обозначить список обекта . мы показывает ктл может с даными работать а кто не может.
+// контекст это один из источников данных откуда функция может черпать информацию
+
+//
+// test;
+timer.startTimer();
+
+// lose context вызов функции вне обэкта.
+
+// const startTimerFunc = timer.startTimer;
+// startTimerFunc();
+
+// set interval exampl
+
+// setInterval(() => {
+//   console.log("Monday");
+// }, 2000);
+
+// lose context case 2
+
+// lose context case #2
+// [1, 2, 4].filter(function () {
+//   console.log("filter this " + this);
+// });
+
+//
