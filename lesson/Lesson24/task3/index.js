91b127dfa9a9d6f97a180858e373aed7169c3e27
svlanakom <svetakom.222@gmail.com>
2021-09-14 13:56:30 +0300

    Lesson24task3try3

--- lesson/Lesson24/task3/index.js
+++ lesson/Lesson24/task3/index.js
@@ -1,10 +1,25 @@
 export const getDiff = (startDate, endDate) => {
   let resultSeconds = 0;
   if (startDate > endDate) {
-    resultSeconds = new Date(startDate - endDate) / (60 * 60 * 24 * 1000);
+    resultSeconds = new Date(startDate - endDate) / 1000;
   } else {
-    resultSeconds = new Date(endDate - startDate) / (60 * 60 * 24 * 1000);
+    resultSeconds = new Date(endDate - startDate) / 1000;
   }
-};
 
-getDiff(new Date(2019, 11, 5), new Date(2016, 10, 5));
+  console.log(
+    `${Math.floor(resultSeconds / 3600 / 24)}d ${
+      Math.floor(resultSeconds / 3600) % 24
+    }h ${Math.floor(resultSeconds / 60) % 60}m ${
+      Math.floor(resultSeconds) % 60
+    }s`
+  );
+  return `${Math.floor(resultSeconds / 3600 / 24)}d ${
+    Math.floor(resultSeconds / 3600) % 24
+  }h ${Math.floor(resultSeconds / 60) % 60}m ${
+    Math.floor(resultSeconds) % 60
+  }s`;
+};
+getDiff(
+  new Date(2012, 11, 3, 11, 21, 2, 0),
+  new Date(2011, 10, 2, 23, 11, 3, 0)
+);
