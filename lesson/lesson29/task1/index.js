08686909f17ca29f448412d6f9d9c400ca34649c
svlanakom <svetakom.222@gmail.com>
2021-09-16 14:37:36 +0300

    Lesson29task1.4

--- lesson/lesson29/task1/index.js
+++ lesson/lesson29/task1/index.js
@@ -27,7 +27,7 @@
 //   onImageLoaded
 // );
 
-const addImage = (imgSrc, callback) => {
+export const addImage = (imgSrc, callback) => {
   const imgElem = document.createElement("img");
   imgElem.setAttribute("alt", "My photo");
   imgElem.src = imgSrc;
