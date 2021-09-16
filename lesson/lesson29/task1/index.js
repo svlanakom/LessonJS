3b813d6d8994592028957b49dc2e596742d94972
svlanakom <svetakom.222@gmail.com>
2021-09-16 15:31:40 +0300

    Lesson29task1.12

--- lesson/lesson29/task1/index.js
+++ lesson/lesson29/task1/index.js
@@ -13,13 +13,15 @@ export const addImage = (imgSrc, callback) => {
   imgElem.addEventListener("load", onImageLoaded);
   imgElem.addEventListener("error", () => callback("Image load is failed"));
 };
+const imgSrc =
+  "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg";
 
-const onImageLoaded = (error, imgElem) => {
+const onImageLoaded = (error, data) => {
   if (error) {
     console.log(error);
     return;
   }
-  const { width, height } = imgElem;
+  const { width, height } = data;
   const sizeElem = document.querySelector(".image-size");
 
   sizeElem.textContent = `${width} x ${height}`;
@@ -27,7 +29,7 @@ const onImageLoaded = (error, imgElem) => {
 
 // examples
 // examples
-addImage(
-  "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg",
-  onImageLoaded
-);
+// addImage(
+// "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg",
+//   onImageLoaded
+// );
