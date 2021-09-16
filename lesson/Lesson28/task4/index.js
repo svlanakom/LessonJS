1521ef2060d6218f24b1479bcc429dd4f1df7142
svlanakom <svetakom.222@gmail.com>
2021-09-16 11:25:13 +0300

    Lesson28task4

---
+++ lesson/Lesson28/task4/index.js
@@ -0,0 +1,16 @@
+const add2 = (value) => value + 2;
+const square = (value) => value * value;
+const half = (value) => value / 2;
+
+export const compose =
+  (...funcs) =>
+  (value) =>
+    funcs.reduce((acc, func) => func(acc), value);
+
+// const doEverything = compose(
+//   add2,
+//   square,
+//   half
+// );
+
+// console.log(doEverything(3));
