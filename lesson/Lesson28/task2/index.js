3474c84e18d0136801f613fbb46548d12226698c
svlanakom <svetakom.222@gmail.com>
2021-09-16 10:40:16 +0300

    Lesson28task2.1

--- lesson/Lesson28/task2/index.js
+++ lesson/Lesson28/task2/index.js
@@ -24,7 +24,7 @@ const tree = {
   ],
 };
 
-const markFavorites = (tree, favorites) => {
+export const markFavorites = (tree, favorites) => {
   const isFavorite = favorites.includes(tree.id);
 
   return {
