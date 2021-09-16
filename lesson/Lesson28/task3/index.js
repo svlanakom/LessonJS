e99b4395e70eba352684adf13de18524fbb198f0
svlanakom <svetakom.222@gmail.com>
2021-09-16 11:11:40 +0300

    Lesson28task3

---
+++ lesson/Lesson28/task3/index.js
@@ -0,0 +1,20 @@
+export const mult = (a) => (b) => a * b;
+
+export const twice = (value) => value * 2;
+export const triple = (value) => value * 3;
+
+// const mult = (numberOne) => (numberTwo) => numberOne * numberTwo;
+// const twice = (numberOne) => mult(numberOne)(2);
+// const triple = (numberOne) => mult(numberOne)(3);
+
+// const mult3 = mult(3);
+// const result = mult3(5);
+// console.log(result);
+
+// console.log(twice(4));
+// console.log(triple(4));
+
+// const sum = (a) => (b) => a + b;
+// const add3 = sum(3);
+// const result = add3(5);
+// console.log(result);
