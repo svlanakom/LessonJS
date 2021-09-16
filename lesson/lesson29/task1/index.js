efdb09d65d66885f93d1450d900bb807e68e18ce
svlanakom <svetakom.222@gmail.com>
2021-09-16 14:52:46 +0300

    Lesson29task1.8

--- lesson/lesson29/task1/index.js
+++ lesson/lesson29/task1/index.js
@@ -5,22 +5,27 @@ export const addImage = (imgSrc, callback) => {
   const containerElem = document.querySelector(".page");
   containerElem.append(imgElem);
 
-  imgElem.addEventListener("load", () => callback(null, imgElem));
-  imgElem.addEventListener("error", (e) => callback("Image load is failed"));
+  const onImageLoaded = () => {
+    const { width, height } = imgElem;
+    callback(null, { width, height });
+  };
+
+  imgElem.addEventListener("load", onImageLoaded);
+  imgElem.addEventListener("error", () => callback("Image load is failed"));
 };
 
-const onImageLoaded = (error, imgElem) => {
+const onImageLoaded = (error, data) => {
   if (error) {
     console.log(error);
     return;
   }
-  const { width, height } = imgElem;
+  const { width, height } = data;
   const sizeElem = document.querySelector(".image-size");
 
   sizeElem.textContent = `${width} x ${height}`;
 };
 
-// // examples;
+// // examples
 addImage(
   "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg",
   onImageLoaded
