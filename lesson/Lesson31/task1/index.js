af3f36554bcce9c2e24084c33f16232f382bdfdf
svlanakom <svetakom.222@gmail.com>
2021-09-17 16:54:32 +0300

    Lesson31task1

---
+++ lesson/Lesson31/task1/index.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/**
+ * @param {string} userId
+ * @return {promise}
+ */
+
+export const requestUserData = (userId) => {
+  const request = new Promise((resolve, reject) => {
+    if (userId === "broken") {
+      setTimeout(() => {
+        reject(new Error("User not found"));
+      }, 500);
+    } else {
+      setTimeout(() => {
+        resolve({
+          name: "John",
+          age: 17,
+          userId,
+          email: `${userId}@example.com`,
+        });
+      }, 1000);
+    }
+  });
+  return request;
+};
+
+// userid777
+
+requestUserData("userid777").then((data) => {
+  console.log(data);
+});
+requestUserData("broken").catch((error) => {
+  console.log(error);
+});
