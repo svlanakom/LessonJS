b7b51167ec0d457b80c05604152c334cda3b0b28
svlanakom <svetakom.222@gmail.com>
2021-09-17 12:43:57 +0300

    Lesson30task3

--- lesson/Lesson30/task3/index.js
+++ lesson/Lesson30/task3/index.js
@@ -1,32 +1,30 @@
-/* здесь мы создаем промис, который резолвит объект { name: 'Tom', age: 17 } через 1 секунду */
-const userDataPromise = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve({ name: "Tom", age: 17 });
-  }, 1000);
+/* ===> 1 <=== */
+
+/*
+ * successPromise должен зарезолвить число 67
+ * Ответьте себе на вопрос, какой тип данных имеет переменная successPromise -- object
+ */
+
+const successPromise = new Promise((resolve) => {
+  resolve(67);
 });
 
 /*
- * Выведите в консоль переменную userDataPromise
- * Ответьте себе на вопрос, какой тип данных имеет переменная userDataPromise? object
+ * допишите обработчик успешного промиса (аргументы и тело ф-ции onSuccess)
+ * чтобы в консоль вывелся квадрат числа, которое резолвиться из successPromise
  */
 
-console.log(userDataPromise);
-
-/*
- * Выведите в консоль переменную userData в обработчике промиса
- * Ответьте себе на вопрос, что находится в переменной userData? обьект который прокинули через resolve в коде выше
- */
-userDataPromise.then((userData) => {
-  console.log(userData);
+successPromise.then((number) => {
+  console.log(number * number);
 });
 
 /*
- * подпишитесь на успешное выполнение промиса userDataPromise
- * используйте метод .then
- * в обработчике промиса (ф-ция внутри .then() ) выведите в консоль строку 'My name is Tom. I am 17 years old'
- * Tom и 17 достаньте с данных, которые приходят в ф-цию onSuccess
+ * в обработчике ошибок промиса (ф-ция onError внутри .catch()) выведите в консоль текст 'I am an error'
+ * текст вывелся в консоль?
+ * подумайте почему
+ * потому что мы получили данные, а этот код должен ловить ошибку при исполнении промиса
+ * и работать будет если не сработал метод then
  */
-
-userDataPromise.then((userData) => {
-  console.log(`My name is ${userData.name}. I am ${userData.age} years old`);
+successPromise.catch(() => {
+  console.log("I am an error");
 });
