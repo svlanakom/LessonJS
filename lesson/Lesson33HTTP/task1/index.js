d56c72c46058a4697b29be4c1ae69f927c38c044
svlanakom <svetakom.222@gmail.com>
2021-09-20 11:47:50 +0300

    Lesson33task1

---
+++ lesson/Lesson33HTTP/task1/index.js
@@ -0,0 +1,33 @@
+const userAvatarElem = document.querySelector(".user__avatar");
+const usernameElem = document.querySelector(".user__name");
+const userLocationELem = document.querySelector(".user__location");
+
+const defaultAvatar = "https://avatars3.githubusercontent.com/u10001";
+
+userAvatarElem.src = defaultAvatar;
+
+// https;//api.github.com/users/USERNAME
+
+// fetch and render user data ...
+
+const showUserBtnElem = document.querySelector(".name-form__btn");
+const userNameInput = document.querySelector(".name-form__input");
+
+const fetchUserData = (userName) =>
+  fetch(`https://api.github.com/users/${userName}`).then((response) =>
+    response.json()
+  );
+
+const renderUserData = (userData) => {
+  const { avatar_url, name, location } = userData;
+  userAvatarElem.src = avatar_url;
+  usernameElem.textContent = name;
+  userLocationELem.textContent = location ? `from ${location}` : "";
+};
+
+const onSearchUser = () => {
+  const userName = userNameInput.value;
+  fetchUserData(userName).then((userData) => renderUserData(userData));
+};
+
+showUserBtnElem.addEventListener("click", onSearchUser);
