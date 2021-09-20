65cf064110b436f2ae233824f86c3be1e38eedd1
svlanakom <svetakom.222@gmail.com>
2021-09-20 12:13:06 +0300

    Lesson33task2.1

--- lesson/Lesson33HTTP/task2/index.js
+++ lesson/Lesson33HTTP/task2/index.js
@@ -1,18 +1,18 @@
 const baseUrl = "https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks";
 
-export function getTasksList() {
+function getTasksList() {
   return fetch(baseUrl).then((response) => response.json());
 }
 
-export function getTaskById(taskId) {
+function getTaskById(taskId) {
   return fetch(`${baseUrl}/${taskId}`).then((response) => response.json());
 }
 
-// examples
-getTasksList().then((tasksList) => {
-  console.log(tasksList); // array of the task objects - [ {'id':'1', 'done':false ... }, {'id':'2', 'done':true ... }, ...]
-});
+// // examples
+// getTasksList().then((tasksList) => {
+//   console.log(tasksList); // array of the task objects - [ {'id':'1', 'done':false ... }, {'id':'2', 'done':true ... }, ...]
+// });
 
-getTaskById("2").then((taskData) => {
-  console.log(taskData); // {'id':'2', 'done':true ... }
-});
+// getTaskById("2").then((taskData) => {
+//   console.log(taskData); // {'id':'2', 'done':true ... }
+// });
