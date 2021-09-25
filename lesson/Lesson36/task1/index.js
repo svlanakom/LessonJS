71b5420201022120832f27edd39cb30ceffa3db9
svlanakom <svetakom.222@gmail.com>
2021-09-25 14:04:03 +0300

    Lesson36task1.2

--- lesson/Lesson36/task1/index.js
+++ lesson/Lesson36/task1/index.js
@@ -1,17 +1,33 @@
-// eslint-disable-next-line consistent-return
+// // eslint-disable-next-line consistent-return
+// export const fetchUser = async (userId) => {
+//   try {
+//     const response = await fetch(`https://api.github.com/users${userId}`);
+//     if (!response.ok || response.status === 404) {
+//       return null;
+//     }
+//     const userData = await response.json();
+//     return userData;
+//   } catch (err) {
+//     throw new Error("Failed to fetch user");
+//   }
+// };
+
+// fetchUser("facebook");
+// //   .then((userData) => console.log(userData))
+// //   .catch((err) => alert(err.message));
+
 export const fetchUser = async (userId) => {
   try {
-    const response = await fetch(`https://api.github.com/users${userId}`);
+    const response = await fetch(`https://api.github.com/users/${userId}`);
     if (!response.ok || response.status === 404) {
       return null;
     }
     const userData = await response.json();
     return userData;
-  } catch (err) {
-    throw new Error("Failed to fetch user");
+  } catch (e) {
+    throw new Error(`Failed to fetch user`);
   }
 };
 
+// fetchUser("facebook").then((data) => console.log(data));
 fetchUser("facebook");
-//   .then((userData) => console.log(userData))
-//   .catch((err) => alert(err.message));
