b4626f514d58abd5b3fa7aeaeb710af2259837af
svlanakom <svetakom.222@gmail.com>
2021-09-25 14:01:04 +0300

    Lesson36task1.1

--- lesson/Lesson36/task1/index.js
+++ lesson/Lesson36/task1/index.js
@@ -2,7 +2,7 @@
 export const fetchUser = async (userId) => {
   try {
     const response = await fetch(`https://api.github.com/users${userId}`);
-    if (!response.ok) {
+    if (!response.ok || response.status === 404) {
       return null;
     }
     const userData = await response.json();
